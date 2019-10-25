bd9d7c0e69632e1bfb0388cbea0d7e7f760c8b2d
Bill G <gbilly3@gmail.com>
2019-10-25 01:39:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ return r
 function reduce (src, cb, starting){
 
 r = (!!starting) ? starting : src[0]
-let i = (!!starting) ? 0 : 1 
+let i = (!!starting) ? 0 : 1
 
 for (; i< src.length; i++){
 
-r = cb (src[i])}
+r = cb (src[i]), r }
 
 
 
